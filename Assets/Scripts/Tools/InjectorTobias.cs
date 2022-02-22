d2895d3e415ee671a5c1cfc9098e2657adf55314
Smiterix <smiterix@gmail.com>
2022-02-22 23:32:46 +0100

    Merge Injector, ToolSwitch Comments

--- Assets/Scripts/Tools/InjectorTobias.cs
+++ Assets/Scripts/Tools/InjectorTobias.cs
@@ -1,26 +1,63 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InjectorTobias : Tool
 {
-
+    [Header("References")]
+    public Text ammoText;
+    public Text slashText;
+    public Text maxAmmoText;
+    
+    [Header("Properties")]
+    public int maxAmmo = 3;
+    public Nullable<int> ammo = null;
     public float healAmount = 0.25f;
-    // Start is called before the first frame update
-    void OnEnable()
-    {
-        GlobalInput.masterInput.Mouse.LeftClick.performed += reduceStress;
+
+    void OnEnable() {
+        // Event
+        GlobalInput.masterInput.Mouse.LeftClick.performed += Inject;
+
+        // Ammo Handling
+        if (ammoText && slashText && maxAmmoText) {
+            // Show Ammo UI
+            changeActiveState(true);
+
+            // Set Ammo values
+            if (ammo == null) ammo = maxAmmo;
+            ammoText.text = ammo.ToString();
+            maxAmmoText.text = maxAmmo.ToString();
+        }
     }
 
-    void OnDisable()
-    {
-        GlobalInput.masterInput.Mouse.LeftClick.performed -= reduceStress;
+    void OnDisable() {
+        // Event
+        GlobalInput.masterInput.Mouse.LeftClick.performed -= Inject;
+
+        // Hide Ammo UI
+        if (ammoText && slashText && maxAmmoText) {
+            changeActiveState(false);
+        }
     }
 
-    void reduceStress(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    void Inject(UnityEngine.InputSystem.InputAction.CallbackContext obj)
     {
-        StressLevel.inst.stressLevel -= .25f;
+        if (ammo > 0) {
+            // StressLevel
+            StressLevel.inst.stressLevel -= healAmount;
+            StressLevel.inst.stressLevel = Mathf.Clamp(StressLevel.inst.stressLevel, 0f, 1f);
 
-        StressLevel.inst.stressLevel = Mathf.Clamp(StressLevel.inst.stressLevel, 0f, 1f);
+            // Ammo Handling
+            ammo--;
+            ammoText.text = ammo.ToString();
+        }
+    }
+
+    void changeActiveState(bool state) {
+        ammoText.gameObject.SetActive(state);
+        slashText.gameObject.SetActive(state);
+        maxAmmoText.gameObject.SetActive(state);
     }
 }
--- Assets/Scripts/Tools/ToolSwitch.cs
+++ Assets/Scripts/Tools/ToolSwitch.cs
@@ -5,87 +5,27 @@ using UnityEngine;
 
 public class ToolSwitch : MonoBehaviour
 { 
-    // public int selectedItem = 0;
-    // private List<Transform> childs = new List<Transform>();
-    // // private Transform[] childs = new Transform[3];
-
-    // void Start()
-    // {
-    //     GlobalInput.masterInput.Mouse.Scroll.performed += ctx => Scrolled(ctx.ReadValue<float>());
-    //     foreach (Transform item in transform) {
-    //         childs.Add(item);
-    //     }
-    //     // int counter = 0;
-    //     // foreach (Transform item in transform) {
-    //     //     childs[counter] = item;
-    //     // }
-    //     SelectItem();
-    // }
-
-    // void Scrolled (float scroll) {
-    //     // int previousSelectedItem = selectedItem;
-
-    //     // if (scroll > 0f) {
-    //     //     if (selectedItem >= transform.childCount - 1) {
-    //     //         selectedItem = 0;
-    //     //     } else {
-    //     //         selectedItem++;
-    //     //     }
-    //     // } else if (scroll < 0f) {
-    //     //     if (selectedItem <= 0) {
-    //     //         selectedItem = transform.childCount - 1;
-    //     //     } else {
-    //     //         selectedItem--;
-    //     //     }
-    //     // }
-
-    //     // if (previousSelectedItem != selectedItem) SelectItem();
-    //     Transform previousSelectedItem = childs[selectedItem];
-
-    //     if (scroll > 0f) {
-    //         if (selectedItem >= transform.childCount - 1) {
-    //             selectedItem = 0;
-    //         } else {
-    //             selectedItem++;
-    //         }
-    //     } else if (scroll < 0f) {
-    //         if (selectedItem <= 0) {
-    //             selectedItem = transform.childCount - 1;
-    //         } else {
-    //             selectedItem--;
-    //         }
-    //     }
-
-    //     if (previousSelectedItem != selectedItem) SelectItem();
-    // }
-
-    // void SelectItem () {
-    //     int counter = 0;
-    //     foreach (Transform item in childs) {
-    //         if (counter == selectedItem && item.GetComponent<Tool>().unlocked) {
-    //             item.gameObject.SetActive(true);
-    //         } else {
-    //             item.gameObject.SetActive(false);
-    //         }
-    //         counter++;
-    //     }
-    // }
     private Transform selectedItem;
     private List<Transform> childs = new List<Transform>();
 
     void Start()
     {
+        // Event
         GlobalInput.masterInput.Mouse.Scroll.performed += ctx => Scrolled(ctx.ReadValue<float>());
+
+        // Tool Childs
         foreach (Transform item in transform) {
             if (item.GetComponent<Tool>().unlocked) {
                 childs.Add(item);
             }
         }
         
+        // Set first unlocked Child
         if (childs.Count > 0) {
             selectedItem = childs[0];
         }
-
+        
+        // Select first unlocked Child
         SelectItem();
     }
 
