8daf6f202593a31f2889e44c9721fbfcd4723c78
TwoBeerGames <twobeergames@gmail.com>
2022-02-25 02:02:03 +0100

    w

---
+++ Assets/Scripts/MonsterAudio.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterAudio : MonoBehaviour
+{
+    AudioSource audioSource;
+    public float interval;
+    public AudioClip[] clips;
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        StartCoroutine(processAudio());
+    }
+
+    IEnumerator processAudio()
+    {
+        float timeElapsed = 0f;
+
+        while (true)
+        {
+            if (timeElapsed <= interval)
+            {
+                timeElapsed += Time.deltaTime;
+                yield return null;
+                continue;
+            }
+            timeElapsed = 0f;
+            audioSource.PlayOneShot(clips[Random.Range(0, clips.Length)]);
+        }
+    }
+}
